5a79a28b51afc6a7c0474f7405fba0114124d63e
OkanLimitless <okangalatasaray001@gmail.com>
2025-01-21 20:14:02 +0100

    new video

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react';
 
 const VideoPlayer = () => {
   useEffect(() => {
-    // Load the smartplayer script
+    // Load the smartplayer script for the new video
     const script = document.createElement('script');
-    script.src = 'https://scripts.converteai.net/fa037796-870e-4d05-a5a0-79244e13ce7f/players/6782768082fbade93fa5911a/player.js';
+    script.src = 'https://scripts.converteai.net/3b5f2e0a-d4d5-4b09-832d-cc04a06c6b74/players/678fe5eedc2aaba0992bb0f1/player.js';
     script.async = true;
-    script.id = 'scr_6782768082fbade93fa5911a';
+    script.id = 'scr_678fe5eedc2aaba0992bb0f1';
     document.head.appendChild(script);
 
     return () => {
@@ -17,7 +17,36 @@ const VideoPlayer = () => {
   return (
     <div className="video-section">
       <div className="video-container">
-        <div id="vid_6782768082fbade93fa5911a" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }} />
+        <div 
+          id="vid_678fe5eedc2aaba0992bb0f1" 
+          style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}
+        >
+          <img 
+            id="thumb_678fe5eedc2aaba0992bb0f1"
+            src="https://images.converteai.net/3b5f2e0a-d4d5-4b09-832d-cc04a06c6b74/players/678fe5eedc2aaba0992bb0f1/thumbnail.jpg"
+            style={{ 
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              display: 'block'
+            }}
+            alt="thumbnail"
+          />
+          <div 
+            id="backdrop_678fe5eedc2aaba0992bb0f1"
+            style={{
+              WebkitBackdropFilter: 'blur(5px)',
+              backdropFilter: 'blur(5px)',
+              position: 'absolute',
+              top: 0,
+              height: '100%',
+              width: '100%'
+            }}
+          />
+        </div>
       </div>
     </div>
   );
