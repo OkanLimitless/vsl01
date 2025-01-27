3a9fc4bb967dc1f2b47ed344e4380640b523537b
OkanLimitless <okangalatasaray001@gmail.com>
2025-01-27 13:57:18 +0100

    new video

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-const VideoPlayer = () => {
+export default function VideoPlayer() {
   useEffect(() => {
     // Set up observer for CTA element
     const observerInterval = setInterval(() => {
@@ -13,9 +13,9 @@ const VideoPlayer = () => {
 
     // Load the smartplayer script for the new video
     const script = document.createElement('script');
-    script.src = 'https://scripts.converteai.net/3b5f2e0a-d4d5-4b09-832d-cc04a06c6b74/players/6790dfc60f8856647ba39eee/player.js';
+    script.src = 'https://scripts.converteai.net/3b5f2e0a-d4d5-4b09-832d-cc04a06c6b74/players/67977ece921b5eb7e8735d5e/player.js';
     script.async = true;
-    script.id = 'scr_6790dfc60f8856647ba39eee';
+    script.id = 'scr_67977ece921b5eb7e8735d5e';
     document.head.appendChild(script);
 
     return () => {
@@ -25,41 +25,32 @@ const VideoPlayer = () => {
   }, []);
 
   return (
-    <div className="video-section">
-      <div className="video-container">
+    <div className="video-wrapper">
+      <div id="vid_67977ece921b5eb7e8735d5e" style={{position: 'relative', width: '100%', padding: '56.25% 0 0'}}>
+        <img 
+          id="thumb_67977ece921b5eb7e8735d5e" 
+          src="https://images.converteai.net/3b5f2e0a-d4d5-4b09-832d-cc04a06c6b74/players/67977ece921b5eb7e8735d5e/thumbnail.jpg" 
+          style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block'}}
+          alt="thumbnail"
+        />
         <div 
-          id="vid_6790dfc60f8856647ba39eee"
-          style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}
-        >
-          <img
-            id="thumb_6790dfc60f8856647ba39eee"
-            src="https://images.converteai.net/3b5f2e0a-d4d5-4b09-832d-cc04a06c6b74/players/6790dfc60f8856647ba39eee/thumbnail.jpg"
-            style={{ 
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              display: 'block'
-            }}
-            alt="thumbnail"
-          />
-          <div 
-            id="backdrop_6790dfc60f8856647ba39eee"
-            style={{
-              WebkitBackdropFilter: 'blur(5px)',
-              backdropFilter: 'blur(5px)',
-              position: 'absolute',
-              top: 0,
-              height: '100%',
-              width: '100%'
-            }}
-          />
-        </div>
+          id="backdrop_67977ece921b5eb7e8735d5e" 
+          style={{
+            WebkitBackdropFilter: 'blur(5px)',
+            backdropFilter: 'blur(5px)',
+            position: 'absolute',
+            top: 0,
+            height: '100%',
+            width: '100%'
+          }}
+        />
       </div>
+      <script
+        id="scr_67977ece921b5eb7e8735d5e"
+        dangerouslySetInnerHTML={{
+          __html: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/3b5f2e0a-d4d5-4b09-832d-cc04a06c6b74/players/67977ece921b5eb7e8735d5e/player.js", s.async=!0,document.head.appendChild(s);`
+        }}
+      />
     </div>
   );
-};
-
-export default VideoPlayer;
+}
