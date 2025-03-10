6a012d29988898572fbdb8c8b00459459a5dd280
OkanLimitless <okangalatasaray001@gmail.com>
2025-03-10 09:02:14 +0100

    2

--- components/TestimonialVideo.js
+++ components/TestimonialVideo.js
@@ -23,29 +23,9 @@ export default function TestimonialVideo() {
   }, []);
 
   return (
-    <div className="testimonial-video-wrapper">
-      <div id={`vid_${videoId}`} style={{position: 'relative', width: '100%', padding: '56.25% 0 0'}}>
-        <img 
-          id={`thumb_${videoId}`} 
-          src={`https://images.converteai.net/0b62a3c4-d373-4d44-b808-36e366f23f00/players/${videoId}/thumbnail.jpg`}
-          style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block', overflow: 'hidden'}}
-          alt="Testimonial Video Thumbnail"
-        />
-        <div 
-          id={`backdrop_${videoId}`} 
-          style={{WebkitBackdropFilter: 'blur(5px)', backdropFilter: 'blur(5px)', position: 'absolute', top: 0, height: '100%', width: '100%'}}
-        ></div>
-      </div>
-
-      <style jsx>{`
-        .testimonial-video-wrapper {
-          width: 100%;
-          max-width: 800px;
-          margin: 20px auto;
-          overflow: hidden;
-          background-color: #000;
-        }
-      `}</style>
+    <div id={`vid_${videoId}`} style={{position: 'relative', width: '100%', padding: '56.25% 0 0'}}> 
+      <img id={`thumb_${videoId}`} src={`https://images.converteai.net/0b62a3c4-d373-4d44-b808-36e366f23f00/players/${videoId}/thumbnail.jpg`} style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block'}} alt="thumbnail" /> 
+      <div id={`backdrop_${videoId}`} style={{WebkitBackdropFilter: 'blur(5px)', backdropFilter: 'blur(5px)', position: 'absolute', top: 0, height: '100%', width: '100%'}}></div> 
     </div>
   );
 } 
